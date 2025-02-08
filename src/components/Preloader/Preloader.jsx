95cec53f212d25f3279a00dc3bb852c5f5536c52
Ebrahim Nasser <ebrahimm.nasser@gmail.com>
2025-02-08 19:49:08 +0200

    edit preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -8,7 +8,7 @@ const Preloader = () => {
   }, []);
   return (
     <div className=" preloader h-screen w-full bg-slate-900 text-slate-200 fixed bottom-0 left-0 right-0 z-50 flex justify-center items-center overflow-hidden">
-      <h1 className=" texts-container flex items-center justify-between h-16 w-[350px] sm:w-96 text-3xl font-bold overflow-hidden">
+      <h1 className=" texts-container flex items-center justify-center gap-1 sm:justify-between h-16 w-full  sm:w-96 text-3xl font-bold overflow-hidden">
         <span>Tiba Academic</span>
         <span>Gateway</span>
       </h1>
